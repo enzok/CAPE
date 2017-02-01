c2b1c90feb585a103f451b79710b8c33664bb7a6
me <me@cuckoo01>
2017-02-01 15:15:58 -0500

    update elf.py and add curl.exe binary

--- analyzer/windows/modules/packages/elf.py
+++ analyzer/windows/modules/packages/elf.py
@@ -2,9 +2,14 @@ from lib.common.abstracts import Package
 
 
 class ELF(Package):
-    """ Dump linux ELF file information to file.
-
+    """Generic analysis package.
+    The sample is started using START command in a cmd.exe prompt.
     """
+    PATHS = [
+        ("SystemRoot", "system32", "cmd.exe"),
+    ]
+
     def start(self, path):
-        out_file = "readelf_output.txt"
-        return self.execute("bin\\readelf.exe", "-a {0} > {1}".format(path, out_file), path)
+        cmd_path = self.get_path("cmd.exe")
+        cmd_args = "/c start /wait \"\" \"echo This is a Linux ELF file.\""
+        return self.execute(cmd_path, cmd_args, path)
