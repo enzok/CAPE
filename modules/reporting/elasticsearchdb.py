63d434d94b124c1a056d9fab31aa8c9bb0ebd1e7
me <me@cuckoo01>
2017-05-21 17:58:08 -0400

    Increase timeout and increase field limit

--- modules/reporting/elasticsearchdb.py
+++ modules/reporting/elasticsearchdb.py
@@ -143,11 +143,9 @@ class ElasticsearchDB(Report):
         settings = {}
         settings["settings"] = {"index": {"mapping": {"total_fields": {"limit": "5000"}}}}
         if self.es.indices.exists(index=self.index_name):
-            value = self.es.indices.get_settings(index=self.index_name, name="index.mapping.total_fields.limit")
-            if "5000" not in value:
-                self.es.indices.put_settings(index=self.index_name, body=settings)
-        else:
-            self.es.indices.create(index=self.index_name, body=settings)
+            self.es.delete(index=self.index_name, doc_type="analysis", id=results["info"]["id"])
+
+        self.es.indices.create(index=self.index_name, body=settings)
 
         # Store the report and retrieve its object id.
         self.es.index(index=self.index_name, doc_type="analysis", id=results["info"]["id"], body=report)
