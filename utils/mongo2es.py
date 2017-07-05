b92a2519e64cb6460e748954c67cf8695b125954
me <me@cuckoo01>
2017-07-05 15:08:20 -0400

    utility to migrate report data from mongo to es

---
+++ utils/mongo2es.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# Copyright (C) 2010-2015 Cuckoo Foundation.
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
+import os
+import sys
+import logging
+import argparse
+import zlib
+import json
+
+log = logging.getLogger()
+
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), ".."))
+
+from lib.cuckoo.common.config import Config
+from lib.cuckoo.common.constants import CUCKOO_ROOT
+from lib.cuckoo.core.database import Database
+from lib.cuckoo.core.startup import ConsoleHandler
+
+
+repconf = Config("reporting")
+if repconf.mongodb.enabled:
+    from bson.objectid import ObjectId
+    from pymongo import MongoClient
+    host = repconf.mongodb.host
+    port = repconf.mongodb.port
+    db = repconf.mongodb.db
+    conn = MongoClient(host, port)
+    mdata = conn[db]
+else:
+    print("Mongodb not configured, exiting...")
+    sys.exit(1)
+
+if repconf.elasticsearchdb.enabled and repconf.elasticsearchdb.searchonly:
+    from elasticsearch import Elasticsearch
+    baseidx = repconf.elasticsearchdb.index
+    es = Elasticsearch(hosts=[{"host": repconf.elasticsearchdb.host,
+                               "port": repconf.elasticsearchdb.port, }],
+                       timeout=300)
+else:
+    print("Elasticsearch not configured, exiting...")
+    sys.exit(1)
+
+
+def process(task):
+    task_id = task.to_dict()["id"]
+
+    analyses = mdata.analysis.find({"info.id": int(task_id)})
+    log.debug("Found %d analyses for %s" % (analyses.count(), task_id))
+    if analyses.count() > 0:
+        log.debug("Retrieving analysis data for Task %s" % task_id)
+        for results in analyses:
+            report = {}
+            idxdate = results["info"]["started"].split(" ")[0]
+            index_name = '{}-{}'.format(baseidx, idxdate)
+            report["task_id"] = results["info"]["id"]
+            report["info"] = results.get("info")
+            report["target"] = results.get("target")
+            report["summary"] = results.get("behavior", {}).get("summary")
+            if report["summary"]:
+                try:
+                    report["summary"] = json.loads(zlib.decompress(report["summary"]))
+                except Exception as err:
+                    log.debug("Error decompressing summary results: %s", err)
+                    pass
+            report["network"] = results.get("network")
+            report["malfamily"] = results.get("malfamily", "")
+            report["cape"] = results.get("cape", "")
+            if report["cape"]:
+                try:
+                    report["cape"] = json.loads(zlib.decompress(report["cape"]))
+                except Exception as err:
+                    log.debug("Error decompressing CAPE results: %s", err)
+                    pass
+            report["virustotal"] = results.get("virustotal")
+            if report["virustotal"]:
+                try:
+                    report["virustotal"] = json.loads(zlib.decompress(report["virustotal"]))
+                except Exception as err:
+                    log.debug("Error decompressing virustotal results: %s", err)
+                    pass
+
+            if "virustotal" in results and results["virustotal"] and "positives" in results["virustotal"] and "total" in results["virustotal"]:
+                report["virustotal_summary"] = "{}/{}".format(results["virustotal"]["positives"],
+                                                              results["virustotal"]["total"])
+            log.debug(report)
+
+            # Create index and set maximum fields limit to 5000
+            settings = {}
+            settings["settings"] = {"index": {"mapping": {"total_fields": {"limit": "5000"}}}}
+
+            if es.indices.exists(index=index_name):
+                es.indices.put_settings(index=index_name, body=settings)
+            else:
+                es.indices.create(index=index_name, body=settings)
+
+            # Store the report
+            es.index(index=index_name, doc_type="analysis", id=report["task_id"], body=report)
+
+
+def init_logging(tid=0, debug=False):
+    formatter = logging.Formatter("%(asctime)s [%(name)s] %(levelname)s: %(message)s")
+    ch = ConsoleHandler()
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
+    
+    fh = logging.handlers.WatchedFileHandler(os.path.join(CUCKOO_ROOT, "log", "es-migrate-%s.log" % tid))
+
+    fh.setFormatter(formatter)
+    log.addHandler(fh)
+
+    if debug:
+        log.setLevel(logging.DEBUG)
+    else:
+        log.setLevel(logging.INFO)
+
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("id", type=str, help="ID of the analysis to migrate")
+    parser.add_argument("-d", "--debug", help="Display debug messages", action="store_true", required=False)
+    args = parser.parse_args()
+
+    init_logging(tid=args.id, debug=args.debug)
+    task = Database().view_task(int(args.id))
+    try:
+        if task:
+            process(task)
+            conn.close()
+        else:
+            log.info("Task does not exist.")
+    except Exception as err:
+        log.exception("Did not migrate task: %s", err)
+
+if __name__ == "__main__":
+    cfg = Config()
+
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
