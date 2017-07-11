bd8c2e717db88dc79c9f63c2198c0625a4ef6a2c
me <me@cuckoo01>
2017-07-11 10:37:12 -0400

    make sure sample has .dll extension

--- analyzer/windows/modules/packages/odbcconf.py
+++ analyzer/windows/modules/packages/odbcconf.py
@@ -1,8 +1,10 @@
 import logging
+import os
 
 from lib.common.abstracts import Package
 
 log = logging.getLogger(__name__)
+
 
 class ODBCCONF(Package):
     """Odbcconf.exe regsvr analysis package."""
@@ -12,5 +14,9 @@ class ODBCCONF(Package):
 
     def start(self, path):
         odbcconf = self.get_path("odbcconf.exe")
+
+        if not path.endswith("dll"):
+            os.rename(path, path + ".dll")
+            path += ".dll"
 
         return self.execute(odbcconf, "/A {REGSVR \"%s\"}" % path, path)
