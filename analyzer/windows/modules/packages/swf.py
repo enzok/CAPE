c6bf9dd21facac215628f5d1383e2cc617500379
me <me@cuckoo01>
2017-10-11 14:52:14 -0400

    process flash with ie plugin

--- analyzer/windows/modules/packages/swf.py
+++ analyzer/windows/modules/packages/swf.py
@@ -2,17 +2,105 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
+import shutil
+import logging
+
+from _winreg import HKEY_LOCAL_MACHINE, HKEY_CURRENT_USER
+
 from lib.common.abstracts import Package
 
-class SWF(Package):
-    """Shockwave Flash analysis package.
-        Download a version of standalone flash from adobe and 
-        place in bin/ as flashplayer.exe to use
+log = logging.getLogger(__name__)
 
-        You can find the bundles you want here: 
-        https://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
-        You want the debug folder (in the zip). The filename will have 'sa' (stand-alone) in it
-    """
+class IE(Package):
+    """Internet Explorer analysis package."""
+    PATHS = [
+        ("ProgramFiles", "Internet Explorer", "iexplore.exe"),
+    ]
+
+    REGKEYS = [
+        [
+            HKEY_CURRENT_USER,
+            "Software\\Microsoft\\Internet Explorer\\Main",
+            {
+                # "Would you like Internet Explorer as default browser?"
+                "Check_Associations": "no",
+
+                # "Set Up Windows Internet Explorer 8"
+                "DisableFirstRunCustomize": 1,
+            },
+        ],
+        [
+            HKEY_CURRENT_USER,
+            "Software\\Microsoft\\Internet Explorer\\Security",
+            {
+                "Safety Warning Level": "Low",
+                "Sending_Security": "Low",
+                "Viewing_Security": "Low",
+            },
+        ],
+        [
+            HKEY_LOCAL_MACHINE,
+            "Software\\Microsoft\\Internet Explorer\\Main",
+            {
+                # Disable Security Settings Check.
+                "DisableSecuritySettingsCheck": 1,
+            },
+        ],
+        [
+            HKEY_CURRENT_USER,
+            "Software\\Microsoft\\Internet Explorer\\Main\\FeatureControl",
+            {
+                "FEATURE_LOCALMACHINE_LOCKDOWN": {
+                    # "To help protect your security, Internet Explorer has
+                    # restricted this webpage from running scripts or ActiveX
+                    # controls that could access your computer. Click here for
+                    # options..."
+                    "iexplore.exe": 0,
+                },
+                "FEATURE_RESTRICT_FILEDOWNLOAD": {
+                    # "To help protect your security, Windows Internet
+                    # Explorer blocked this site from downloading files to
+                    # your computer. Click here for more options..."
+                    "iexplore.exe": 0,
+                },
+            },
+        ],
+        [
+            HKEY_CURRENT_USER,
+            "Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings",
+            {
+                # "You are about to be redirected to a connection that is not secure."
+                "WarnOnHTTPSToHTTPRedirect": 0,
+
+                # "You are about to view pages over a secure connection."
+                "WarnOnZoneCrossing": 0,
+            },
+        ],
+        [
+            HKEY_CURRENT_USER,
+            "Software\\Microsoft\\Internet Explorer\\Document Windows",
+            {
+                # Maximize the window by default.
+                "Maximized": "yes",
+            },
+        ],
+        [
+            HKEY_CURRENT_USER,
+            "Software\\Microsoft\\Internet Explorer\\Download",
+            {
+                # "Internet Explorer - Security Warning"
+                # "The publisher could not be verified."
+                "CheckExeSignatures": "no",
+            },
+        ],
+    ]
 
     def start(self, path):
-        return self.execute("bin\\flashplayer.exe", path, path)
+        iexplore = self.get_path("Internet Explorer")
+
+        if not path.endswith((".swf")):
+            shutil.copy(path, path + ".swf")
+            path += ".swf"
+            log.info("Submitted file is missing extension, adding .swf")
+
+        return self.execute(iexplore, "\"%s\"" % path, path)
