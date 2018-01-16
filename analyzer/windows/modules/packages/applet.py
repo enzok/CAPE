b3c57c9cd4c61667b5b4c57a2db9202e9fba014b
me <me@cuckoo01>
2018-01-16 11:30:51 -0500

    add params to applet package WIP

--- analyzer/windows/modules/packages/applet.py
+++ analyzer/windows/modules/packages/applet.py
@@ -3,6 +3,7 @@
 # See the file 'docs/LICENSE' for copying permission.
 
 import tempfile
+from json import loads
 
 from lib.common.abstracts import Package
 
@@ -13,15 +14,27 @@ class Applet(Package):
         ("ProgramFiles", "Internet Explorer", "iexplore.exe"),
     ]
 
-    def make_html(self, path, class_name):
-        html = """
+    def make_html(self, path, class_name, param_list):
+        params = ""
+
+        if param_list:
+            params = loads(param_list)
+            for name in params:
+                params += """
+                <param name="%s" value="%s" />
+                """ % (name, param_list[name])
+        html_start = """
         <html>
             <body>
                 <applet archive="%s" code="%s" width="1" height="1">
+        """ % (path, class_name)
+        html_end = """
                 </applet>
             </body>
         </html>
-        """ % (path, class_name)
+        """
+
+        html = html_start + params + html_end
 
         _, file_path = tempfile.mkstemp(suffix=".html")
         with open(file_path, "w") as file_handle:
@@ -32,5 +45,6 @@ class Applet(Package):
     def start(self, path):
         browser = self.get_path("browser")
         class_name = self.options.get("class")
-        html_path = self.make_html(path, class_name)
+        param_list = self.options.get("applet_params")
+        html_path = self.make_html(path, class_name, param_list)
         return self.execute(browser, "\"%s\"" % html_path, html_path)
