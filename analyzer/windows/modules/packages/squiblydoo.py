f9bae59369ed37394b50b1462f66c870a43eaf23
me <me@cuckoo01>
2018-08-07 11:25:40 -0400

    Add squiblydoo package

---
+++ analyzer/windows/modules/packages/squiblydoo.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2010-2015 Cuckoo Foundation.
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
+import os
+import shutil
+
+from lib.common.abstracts import Package
+
+class Regsvr(Package):
+    """Squiblydoo analysis package."""
+    PATHS = [
+        ("SystemRoot", "system32", "regsvr32.exe"),
+    ]
+
+    def start(self, path):
+        regsvr32 = self.get_path("regsvr32.exe")
+        args = "\"/i:{0}\" scrobj.dll".format(path)
+
+        return self.execute(regsvr32, args, path)
