14a1cde0527d624d253bd21e873d597571557605
doomedraven <doomedraven@users.noreply.github.com>
2018-10-16 06:08:48 -0400

    improve yara_detected in abstracts.py
    (cherry picked from commit b052ca4150ff44eb90c0384849f6eddeb525589f)

--- lib/cuckoo/common/abstracts.py
+++ lib/cuckoo/common/abstracts.py
@@ -709,15 +709,19 @@ class Signature(object):
 
         for keyword in ("procmemory", "extracted", "dropped", "CAPE"):
             for block in self.results.get(keyword, []):
-                if re.findall(name, block["yara"]["name"], re.I):
-                    if keyword in ("dropped", "extracted"):
-                        path = block["path"]
-                    elif keyword == "procmem":
-                        path = block["file"]
-                    elif keyword == "CAPE":
-                        path = block["raw"]
+                for sub_block in block["yara"]:
+                    if re.findall(name, sub_block["name"], re.I):
+                        if keyword in ("dropped", "extracted", "procmemory"):
+                            if block["file"]:
+                                path = block["file"]
+                            else:
+                                path = block["path"]
+                        elif keyword == "CAPE":
+                            path = block["raw"]
+                        else:
+                            path = ""
 
-                    return keyword, path, block["yara"]
+                        return keyword, path, sub_block
 
         return False, False, False
 
