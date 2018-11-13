73f3f3c6ff880e62ba315646d882fe2270a0e8ee
me <me@cuckoo01>
2018-11-13 08:48:16 -0500

    update sct package description

--- analyzer/windows/modules/packages/sct.py
+++ analyzer/windows/modules/packages/sct.py
@@ -15,6 +15,6 @@ class SCT(Package):
 
     def start(self, path):
         regsvr32 = self.get_path("regsvr32.exe")
-        args = "\"/i:{0}\" scrobj.dll".format(path)
+        args = " {0}".format(path)
 
         return self.execute(regsvr32, args, path)
