07a355e4f6d33fdb732194b6efeee4f0e90c908a
me <me@cuckoo01>
2018-12-11 20:40:32 -0500

    Experimental signature

--- modules/signatures/suspicious_com_guids.py
+++ modules/signatures/suspicious_com_guids.py
@@ -14,7 +14,19 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from lib.cuckoo.common.abstracts import Signature
+from lib.cuckoo.common.constants import CUCKOO_ROOT
 
+
+# see https://raw.githubusercontent.com/honeynet/cuckooml/master/data/guids.txt
+
+with open(CUCKOO_ROOT+"/data/guid_list.txt", "r") as guidfile:
+    GUIDS = {}
+    for line in guidfile.read().split("\n"):
+        try:
+            guid, desc, info = line.split(" ")
+            GUIDS[guid] = [desc, info]
+        except ValueError:
+            pass
 
 class WMIViaCOMApi(Signature):
     name = "WMI_using_COM_API"
@@ -69,6 +81,34 @@ class IEViaCOMApi(Signature):
                 iid = self.get_argument(call, "riid")
                 if iid == "EAB22AC1-30C1-11CF-A7EB-0000C05BAE0B":
                     self.data.append({"IWebBrowser Interface": "Internet Explorer started using COM interface"})
+
+    def on_complete(self):
+        if self.data:
+            return True
+        else:
+            return False
+
+class COMGUIDs(Signature):
+    name = "suspicious_COM_GUIDs"
+    description = "Suspicious COM GUIDs"
+    severity = 3
+    confidence = 80
+    categories = ["stealth"]
+    authors = ["enzok"]
+    minimum = "1.3"
+    evented = True
+    match = True
+
+    def __init__(self, *args, **kwargs):
+        Signature.__init__(self, *args, **kwargs)
+
+    filter_apinames = set(["CoCreateInstance", "CoGetClassObject", "CoCreateInstanceEx"])
+
+    def on_call(self, call, process):
+        if call['api'] == "CoCreateInstance" or call['api'] == "CoCreateInstanceEx":
+            clsid = self.get_argument(call, "rclsid")
+            if clsid and clsid in GUIDS:
+                self.data.append({desc: info})
 
     def on_complete(self):
         if self.data:
