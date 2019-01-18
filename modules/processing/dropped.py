2cb4d85b213ac63726e5964051d1d59cfe15de85
me <me@cuckoo01>
2019-01-18 09:35:28 -0500

    Test better method to check if text file for text preview in results

--- modules/processing/dropped.py
+++ modules/processing/dropped.py
@@ -3,14 +3,30 @@
 # See the file 'docs/LICENSE' for copying permission.
 
 import os
+import codecs
 
 from lib.cuckoo.common.abstracts import Processing
-from lib.cuckoo.common.config import Config
 from lib.cuckoo.common.objects import File
 from lib.cuckoo.common.utils import convert_to_printable
 
+
 class Dropped(Processing):
     """Dropped files analysis."""
+
+    def _is_text_file(self, file_path):
+        #: BOMs to indicate that a file is a text file even if it contains zero bytes.
+        # https://stackoverflow.com/questions/898669/how-can-i-detect-if-a-file-is-binary-non-text-in-python
+        _TEXT_BOMS = (
+            codecs.BOM_UTF16_BE,
+            codecs.BOM_UTF16_LE,
+            codecs.BOM_UTF32_BE,
+            codecs.BOM_UTF32_LE,
+            codecs.BOM_UTF8,
+        )
+        with open(file_path, 'rb') as source_file:
+            initial_bytes = source_file.read(8192)
+
+        return any(initial_bytes.startswith(bom) for bom in _TEXT_BOMS) and not b'\0' in initial_bytes
 
     def run(self):
         """Run analysis.
@@ -32,19 +48,21 @@ class Dropped(Processing):
                 continue
             guest_paths = [line.strip() for line in open(file_path + "_info.txt")]
             guest_name = guest_paths[0].split("\\")[-1]
-            file_info = File(file_path=file_path,guest_paths=guest_paths, file_name=guest_name).get_all()
-            texttypes = [
+            file_info = File(file_path=file_path, guest_paths=guest_paths, file_name=guest_name).get_all()
+            '''texttypes = [
                 "ASCII",
                 "Windows Registry text",
                 "XML document text",
                 "Unicode text",
+                "UTF-8 Unicode (with BOM) text"
             ]
             readit = False
             for texttype in texttypes:
                 if texttype in file_info["type"]:
                     readit = True
                     break
-            if readit:
+            '''
+            if self._is_text_file(file["path"]):
                 with open(file_info["path"], "r") as drop_open:
                     filedata = drop_open.read(buf + 1)
                 if len(filedata) > buf:
