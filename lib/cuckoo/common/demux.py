a116c6a1111dbc99de4e08e7a76ea1fb0e089eb4
me <me@cuckoo01>
2019-02-19 08:20:13 -0500

    don't limit files that are extracted

--- lib/cuckoo/common/demux.py
+++ lib/cuckoo/common/demux.py
@@ -35,13 +35,6 @@ from lib.cuckoo.common.exceptions import CuckooDemuxError
 logging.basicConfig()
 log = logging.getLogger(__name__)
 
-demux_extensions_list = [
-        "", ".exe", ".dll", ".com", ".jar", ".pdf", ".msi", ".bin", ".scr", ".zip", ".tar", ".gz", ".tgz", ".rar", ".htm", ".html", ".hta",
-        ".doc", ".dot", ".docx", ".dotx", ".docm", ".dotm", ".docb", ".mht", ".mso", ".js", ".jse", ".vbs", ".vbe",
-        ".xls", ".xlt", ".xlm", ".xlsx", ".xltx", ".xlsm", ".xltm", ".xlsb", ".xla", ".xlam", ".xll", ".xlw",
-        ".ppt", ".pot", ".pps", ".pptx", ".pptm", ".potx", ".potm", ".ppam", ".ppsx", ".ppsm", ".sldx", ".sldm", ".wsf",
-    ]
-
 
 def options2passwd(options):
     password = False
@@ -135,10 +128,7 @@ def demux_zip(filename, options):
                 ext = ext.lower()
                 if ext == "" and len(basename) and basename[0] == ".":
                     continue
-                for theext in demux_extensions_list:
-                    if ext == theext:
-                        extracted.append(info.filename)
-                        break
+                extracted.append(info.filename)
 
             if extracted:
                 options = Config()
@@ -194,10 +184,7 @@ def demux_rar(filename, options):
                 ext = ext.lower()
                 if ext == "" and len(basename) and basename[0] == ".":
                     continue
-                for theext in demux_extensions_list:
-                    if ext == theext:
-                        extracted.append(info.filename)
-                        break
+                extracted.append(info.filename)
 
             if extracted:
                 options = Config()
@@ -248,10 +235,7 @@ def demux_tar(filename, options):
                 ext = ext.lower()
                 if ext == "" and len(basename) and basename[0] == ".":
                     continue
-                for theext in demux_extensions_list:
-                    if ext == theext:
-                        extracted.append(info)
-                        break
+                extracted.append(info)
 
             if extracted:
                 options = Config()
