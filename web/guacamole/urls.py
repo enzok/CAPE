de54b329a7c416f3d471fb183a551efe021bd4a5
me <me@cuckoo01>
2019-02-27 15:49:59 -0500

    fix url

--- web/guacamole/urls.py
+++ web/guacamole/urls.py
@@ -3,5 +3,5 @@ from guacamole import views
 
 urlpatterns = [
     url(r'^(?P<task_id>\d+)/$', views.index, name='guacamole'),
-    url(r"^tunnel/(?P<host>\w+)/", views.tunnel, name='tunnel'),
+    url(r"tunnel/(?P<host>\w+)/", views.tunnel, name='tunnel'),
 ]
