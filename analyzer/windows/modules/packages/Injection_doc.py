c5c8101b251b7db526b5a694212b9969e28bc5e1
me <7831008+enzok@users.noreply.github.com>
2019-10-16 13:12:05 -0400

    some more fixes

--- analyzer/windows/modules/packages/Injection_doc.py
+++ analyzer/windows/modules/packages/Injection_doc.py
@@ -2,7 +2,6 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
-import os
 from lib.common.abstracts import Package
 import os
 
