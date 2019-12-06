e862cbe6a032d149012abcd76560f5fd9ec874a0
me <7831008+enzok@users.noreply.github.com>
2019-12-06 15:20:46 -0500

    Add webshell testing package

--- analyzer/windows/modules/packages/zzzWebshell.py
+++ analyzer/windows/modules/packages/zzzWebshell.py
@@ -16,11 +16,11 @@ class Exe(Package):
 
         # copy the webshell to specified directory
         if not wwwroot:
-            wwwroot = os.path.join(wwwroot, "inetpub", "wwwroot")
+            wwwroot = os.path.join("inetpub", "wwwroot")
         basepath = os.getenv('SystemDrive')
-        newpath = os.path.join(basepath, wwwroot)
+        newpath = os.path.join(basepath, wwwroot, os.path.basename(path))
         shutil.copy(path, newpath)
         path = ""
         cmd_path = self.get_path("cmd.exe")
-        cmd_args = "/c start /wait dir \"\" \"{0}\"".format(path)
+        cmd_args = "/c start /wait dir \"\" \"{0}\"".format(newpath)
         return self.execute(cmd_path, cmd_args, path)
