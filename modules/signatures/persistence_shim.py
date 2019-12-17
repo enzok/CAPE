d6bdfdfc30eac80628a9e3b9852d89e7334efa80
me <7831008+enzok@users.noreply.github.com>
2019-12-17 09:10:56 -0500

    fix data type

--- modules/signatures/persistence_shim.py
+++ modules/signatures/persistence_shim.py
@@ -42,13 +42,13 @@ class PersistenceShimDatabase(Signature):
             match = self.check_write_key(pattern=indicator, regex=True)
             if match:
                 ret = True
-                self.data.append({"regkey": list(match) })
+                self.data.append({"regkey": match })
 
         for indicator in file_indicators:
             match = self.check_write_file(pattern=indicator, regex=True, all=True)
             if match:
                 ret = True
-                self.data.append({"file": list(match) })
+                self.data.append({"file": match })
 
         cmdlines = self.results["behavior"]["summary"]["executed_commands"]
         for cmdline in cmdlines:
